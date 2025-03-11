838afc246af3dffcb7950187623c5b0988723dd0
Vedant Jain <vedantj890@gmail.com>
2025-03-11 21:04:00 +0530

    completed

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask , render_template ,jsonify ,request
+from src.helper import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Pinecone 
+import pinecone
+from langchain.chains import RetrievalQA
+from langchain_community.llms import CTransformers  # Corrected import
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+# Load API keys from .env
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# Initialize Pinecone
+pc = pinecone.Pinecone(api_key=PINECONE_API_KEY)
+
+# Define index name
+index_name = "medicalchatbot"
+
+# Loading the index from the pinecone
+docsearch = Pinecone.from_existing_index(index_name , embeddings)
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs={"prompt": PROMPT}
+
+from ctransformers import AutoModelForCausalLM
+
+# Load the model from the local directory
+llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config={'max_new_tokens':512,
+                          'temperature':0.8})
+
+retriever = docsearch.as_retriever(search_kwargs={"k": 1}) # Example: Retrieve only 2 documents
+qa = RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=retriever,
+    return_source_documents=True, 
+    chain_type_kwargs = {"verbose": False}    
+)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa.invoke({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

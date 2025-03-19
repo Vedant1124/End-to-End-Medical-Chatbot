a4488ef4c4f63e95bdfaddef99ee512cc5fcf221
Vedant Jain <vedantj890@gmail.com>
2025-03-19 21:39:02 +0530

    file updated

--- src/prompt.py
+++ src/prompt.py
@@ -2,6 +2,7 @@
 
 prompt_template="""Use the following pieces of information to answer the user's question.
 If you don't know the answer , just say that you dont'know , don't try to make up an answer.
+If user type basic hi and hello query reply with hi only and ask them what can you do for them
 
 Context:{context}
 Question:{question}
